33da54be391d9b387271fde7f684fcaae05c922b
davidlim2007 <lim.david2007@gmail.com>
2017-07-30 19:15:33 +0800

    First commit
    Adding all project files.

---
+++ UIThreadDemo03/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace UIThreadDemo03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateUIThread = new System.Windows.Forms.Button();
+            this.btnAbortThread = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCreateUIThread
+            // 
+            this.btnCreateUIThread.Location = new System.Drawing.Point(12, 164);
+            this.btnCreateUIThread.Name = "btnCreateUIThread";
+            this.btnCreateUIThread.Size = new System.Drawing.Size(258, 30);
+            this.btnCreateUIThread.TabIndex = 0;
+            this.btnCreateUIThread.Text = "Create UI Thread";
+            this.btnCreateUIThread.UseVisualStyleBackColor = true;
+            this.btnCreateUIThread.Click += new System.EventHandler(this.btnCreateUIThread_Click);
+            // 
+            // btnAbortThread
+            // 
+            this.btnAbortThread.Location = new System.Drawing.Point(12, 201);
+            this.btnAbortThread.Name = "btnAbortThread";
+            this.btnAbortThread.Size = new System.Drawing.Size(258, 30);
+            this.btnAbortThread.TabIndex = 1;
+            this.btnAbortThread.Text = "Abort Thread";
+            this.btnAbortThread.UseVisualStyleBackColor = true;
+            this.btnAbortThread.Click += new System.EventHandler(this.btnAbortThread_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.btnAbortThread);
+            this.Controls.Add(this.btnCreateUIThread);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateUIThread;
+        private System.Windows.Forms.Button btnAbortThread;
+    }
+}
+
---
+++ UIThreadDemo03/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIThreadDemo03
+{
+    public partial class Form1 : Form
+    {
+        private Thread m_ui_thread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateUIThread_Click(object sender, EventArgs e)
+        {
+            // The following button handler will initiate a new thread
+            // with UIThread() being the entry point method.
+            m_ui_thread = new Thread(new ThreadStart(UIThread));
+            m_ui_thread.Start();
+        }
+
+        // UIThread() is the entry point method for a thread.
+        private void UIThread()
+        {
+            try
+            {
+                // Note that inside the try block, Application.Run()
+                // is called with a new instance of Form2.
+                // This will start a message loop for the current thread
+                // and will set a new instance of Form2 as the main form
+                // of the thread.
+                // Application.Run() will not return until the instance
+                // of Form2 is closed.
+                Application.Run(new Form2());
+            }
+            catch (ThreadAbortException taex)
+            {
+                // If Thread.Abort() is called on this thread,
+                // A ThreadAbortException exception will be
+                // thrown inside this thread.
+                MessageBox.Show(taex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Exiting thread.");
+            }
+        }
+
+        private void btnAbortThread_Click(object sender, EventArgs e)
+        {
+            if (m_ui_thread != null && m_ui_thread.IsAlive == true)
+            {
+                m_ui_thread.Abort();
+            }
+        }
+    }
+}
